460cbbdb32b80687367c2511e5a82e00135ec0a0
JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>
2025-06-29 15:48:33 +0900

    ✨ [Feat] - 스프링 초기 설정 및 인프라 설정을 한다. (#2)
    * Feat: 스프링 프로젝트 기본 설정을 한다.
    
    * Chore: 도커 이미지 생성 코드
    
    * Feat: helm chart 기본 설정 추가
    
    * feat: value.yaml 공통, 로컬, 배포 상황으로 분리

---
+++ src/main/java/hanium/dongguk/DonggukApplication.java
@@ -0,0 +1,13 @@
+package hanium.dongguk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonggukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonggukApplication.class, args);
+	}
+
+}
