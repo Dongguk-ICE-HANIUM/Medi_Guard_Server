c9e92917ebafb06f30c855c9a575526310314c51
JeongHeumChoi <andyc707@gmail.com>
2025-08-22 17:52:36 +0900

    Chore: swagger를 위한 메소드

--- src/main/java/hanium/dongguk/drug/patientdrug/controller/PatientDrugApiSwagger.java
+++ src/main/java/hanium/dongguk/drug/patientdrug/controller/PatientDrugApiSwagger.java
@@ -1,13 +1,13 @@
 package hanium.dongguk.drug.patientdrug.controller;
 
 import hanium.dongguk.drug.patientdrug.dto.request.CreatePatientDrugRequestDto;
+import hanium.dongguk.drug.patientdrug.dto.request.PatchPatientDrugIsEssentialRequestDto;
 import hanium.dongguk.global.annotation.UserId;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,4 +24,29 @@ public interface PatientDrugApiSwagger {
             @UserId UUID userId,
             @Valid @RequestBody CreatePatientDrugRequestDto requestDto);
 
+    @PatchMapping("/{patientDrugId}")
+    public ResponseEntity<?> patchIsActive(
+            @UserId UUID userId,
+            @PathVariable("patientDrugId")
+            UUID patientDrugId,
+            @Valid @RequestBody
+            PatchPatientDrugIsEssentialRequestDto requestDto)
+
+    @GetMapping("/{patientDrugId}")
+    public ResponseEntity<?> retrievePatientDrug(
+            @UserId UUID userId,
+            @PathVariable("patientDrugId") UUID patientDrugId
+    );
+
+    @DeleteMapping("/{patientDrugId}")
+    public ResponseEntity<?> deletePatientDrug(
+            @UserId UUID userId,
+            @PathVariable("patientDrugId") UUID patientDrugId
+    );
+
+    @PatchMapping("/{patientDrugId}/release")
+    public ResponseEntity<?> detachDrugGoupFromPatientDrug(
+            @UserId UUID userId,
+            @PathVariable("patientDrugId") UUID patientDrugId
+    );
 }
